461ad2f2c54113b71a8aa3d3126971e11bc1c271
kalpeshdhotre <kalpeshdhotre@hotmail.com>
2022-12-22 09:47:12 +0530

    final version 1

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 let userKeyPressed = document.querySelectorAll(`.btn`);
 let stringCaptured = "";
+let btnResult = document.querySelector(".btnResult");
+let btnCancel = document.querySelector(".btnCancel");
 
+let result;
 userKeyPressed.forEach((button) => {
     button.addEventListener(`click`, () => {
         console.log(`${button.innerHTML}`);
-        console.log("Hello");
         stringCaptured += button.innerHTML;
         document.querySelector(`.screen`).value = stringCaptured;
     });
+});
+
+btnResult.addEventListener(`click`, () => {
+    result = eval(stringCaptured);
+    document.querySelector(`.screen`).value = eval(stringCaptured);
+    console.log(result);
+    stringCaptured = result;
+});
+
+btnCancel.addEventListener(`click`, () => {
+    document.querySelector(`.screen`).value = "";
+    result = 0;
+    stringCaptured = "";
 });
 
 // userKeyPressed.addEventListener(`click`, () => {
