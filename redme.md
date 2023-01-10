181e81a2058635b00392982c884ab8e64270ae70
kalpeshdhotre <kalpeshdhotre@hotmail.com>
2023-01-10 18:09:51 +0530

    readme added

---
+++ redme.md
@@ -0,0 +1,5 @@
+by: Kaklpesh K D
+
+# Calculator APP
+
+## [Click here for live link](https://kdjscalculator.netlify.app/)
--- script copy.js
+++ script copy.js
@@ -2,29 +2,15 @@ let userKeyPressed = document.querySelectorAll(`.btn`);
 let stringCaptured = "";
 let btnResult = document.querySelector(".btnResult");
 let btnCancel = document.querySelector(".btnCancel");
+
 let result;
-
-
 userKeyPressed.forEach((button) => {
-    button.addEventListener(`click`, (event) => capture(event));
+    button.addEventListener(`click`, () => {
+        console.log(`${button.innerHTML}`);
+        stringCaptured += button.innerHTML;
+        document.querySelector(`.screen`).value = stringCaptured;
+    });
 });
-
-document.addEventListener(`keydown`, (event)=>capture2(event));
-
-
-function capture(button){
-    console.log(`${button.key}`);
-    // stringCaptured += button.innerHTML;
-    // document.querySelector(`.screen`).value = stringCaptured;    
-}
-
-// function capture(button){
-//     console.log(`${button.innerHTML}`);
-//     stringCaptured += button.innerHTML;
-//     document.querySelector(`.screen`).value = stringCaptured;    
-// }
-
-
 
 btnResult.addEventListener(`click`, () => {
     result = eval(stringCaptured);
