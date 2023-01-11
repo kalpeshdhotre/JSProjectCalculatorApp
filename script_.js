0f3489ab2e32ec130dffc6569e81879ac79d410f
kalpeshdhotre <kalpeshdhotre@hotmail.com>
2023-01-11 09:48:09 +0530

    error

--- script_.js
+++ script_.js
@@ -2,15 +2,29 @@ let userKeyPressed = document.querySelectorAll(`.btn`);
 let stringCaptured = "";
 let btnResult = document.querySelector(".btnResult");
 let btnCancel = document.querySelector(".btnCancel");
-
 let result;
+
+
 userKeyPressed.forEach((button) => {
-    button.addEventListener(`click`, () => {
-        console.log(`${button.innerHTML}`);
-        stringCaptured += button.innerHTML;
-        document.querySelector(`.screen`).value = stringCaptured;
-    });
+    button.addEventListener(`click`, (event) => capture(event));
 });
+
+document.addEventListener(`keydown`, (event)=>capture2(event));
+
+
+function capture(button){
+    console.log(`${button.key}`);
+    // stringCaptured += button.innerHTML;
+    // document.querySelector(`.screen`).value = stringCaptured;    
+}
+
+// function capture(button){
+//     console.log(`${button.innerHTML}`);
+//     stringCaptured += button.innerHTML;
+//     document.querySelector(`.screen`).value = stringCaptured;    
+// }
+
+
 
 btnResult.addEventListener(`click`, () => {
     result = eval(stringCaptured);
