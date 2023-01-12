0a407d4ce26fc924bcbd06415d996a47efcac412
kalpeshdhotre <kalpeshdhotre@hotmail.com>
2023-01-12 16:05:16 +0530

    Keyboard Input also added

--- script.js
+++ script.js
@@ -1,33 +1,54 @@
 let userKeyPressed = document.querySelectorAll(`.btn`);
-let stringCaptured = "";
 let btnResult = document.querySelector(".btnResult");
 let btnCancel = document.querySelector(".btnCancel");
 
+let stringCaptured = "";
 let result;
+
+let keyInputSet = [`.`, `1`, `2`, `3`, `4`, `5`, `6`, `7`, `8`, `9`, `0`, `Escape`, `Enter`, `/`, `*`, `-`, `+`];
+
 userKeyPressed.forEach((button) => {
-    button.addEventListener(`click`, () => {
-        console.log(`${button.innerHTML}`);
-        stringCaptured += button.innerHTML;
-        document.querySelector(`.screen`).value = stringCaptured;
-    });
+   button.addEventListener(`click`, (event) => capture(button.innerHTML));
 });
 
-btnResult.addEventListener(`click`, () => {
-    result = eval(stringCaptured);
-    document.querySelector(`.screen`).value = eval(stringCaptured);
-    console.log(result);
-    stringCaptured = result;
+document.addEventListener(`keydown`, (event) => {
+   console.log(`Key is : ${event.key}`);
+
+   if (keyInputSet.includes(event.key)) {
+      if (event.key == "Escape") {
+         clearScreen();
+         return;
+      } else if (event.key == "Enter") {
+         showResult();
+         return;
+      }
+      capture(event.key);
+   } else {
+      console.log(`non number keys pressed`);
+   }
 });
 
-btnCancel.addEventListener(`click`, () => {
-    document.querySelector(`.screen`).value = "";
-    result = 0;
-    stringCaptured = "";
-});
 
-// userKeyPressed.addEventListener(`click`, () => {
+btnResult.addEventListener(`click`, () => showResult());
 
-//      console.log(`${userKeyPressed.innerHTML}`);
-//     console.log("Hello");
+btnCancel.addEventListener(`click`, () => clearScreen());
 
-// });
+function capture(button) {
+   console.log(`${button}`);
+   stringCaptured += button;
+   document.querySelector(`.screen`).value = stringCaptured;
+}
+
+function showResult() {
+   result = eval(stringCaptured);
+   document.querySelector(`.screen`).value = eval(stringCaptured);
+   console.log(result);
+   stringCaptured = result;
+}
+
+function clearScreen() {
+   document.querySelector(`.screen`).value = "";
+   result = 0;
+   stringCaptured = "";
+}
+
